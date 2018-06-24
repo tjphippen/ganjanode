8d4e8235235442b161e500d51ebeb170b93a4f66
TJ Phippen <tj@tjphippen.com>
2018-06-24 13:57:17 -0700

    fix ports typo

--- ganjainstall.sh
+++ ganjainstall.sh
@@ -4,10 +4,11 @@ echo "-------------------------------------"
 echo
 IPADDR=$(ip route get 8.8.8.8 | awk 'NR==1 {print $NF}')
 read -e -p "VPS IP Address: " -i "$IPADDR" IPADDR
+read -e -p "Choose Port: " -i "12419" MNPORT
 read -e -p "Choose RPC Port: " -i "12420" RPCPORT
 read -rsp $'This will take a while so smoke a bowl & chill out, press any key to continue...\n' -n1 key
 apt-get update && apt-get -y upgrade && apt-get -y dist-upgrade && apt-get -y autoremove && apt-get -y install ufw fail2ban libwww-perl build-essential libtool automake autotools-dev autoconf pkg-config libssl-dev libgmp3-dev libevent-dev bsdmainutils libdb++-dev libminiupnpc-dev libboost-all-dev libqrencode-dev unzip && fallocate -l 4G /swapfile && chmod 600 /swapfile && mkswap /swapfile && swapon /swapfile && echo "vm.swappiness=10" >> /etc/sysctl.conf && echo "/swapfile none swap sw 0 0" >> /etc/fstab
-ufw allow 22/tcp && ufw allow $RPCPORT/tcp && yes | ufw enable
+ufw allow 22/tcp && ufw allow $MNPORT/tcp && yes | ufw enable
 mkdir -p ~/coins && cd ~/coins && git clone https://github.com/legends420/GanjaCoin.git && cd ~/coins/GanjaCoin/src && make -f makefile.unix
 ~/coins/GanjaCoin/src/ganjacoind
 sudo cp ~/coins/GanjaCoin/MNSampleGanjaProject.conf ~/.Ganjaproject2/Ganjaproject.conf
@@ -22,7 +23,7 @@ rpcuser=$RPCUSER
 rpcpassword=$RPCPASS
 rpcallowip=localhost
 rpcport=$RPCPORT
-port=12419
+port=$MNPORT
 externalip=$IPADDR
 server=1
 listen=1
