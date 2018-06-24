4508449b07820566255aab9609b048e774113b91
TJ Phippen <tj@tjphippen.com>
2018-06-24 15:56:10 -0700

    Update ganjainstall.sh

--- ganjainstall.sh
+++ ganjainstall.sh
@@ -8,7 +8,6 @@ read -e -p "Choose Port: " -i "12419" MNPORT
 read -e -p "Choose RPC Port: " -i "12420" RPCPORT
 read -rsp $'This will take a while so smoke a bowl & chill out, press any key to continue...\n' -n1 key
 apt-get update && apt-get -y upgrade && apt-get -y dist-upgrade && apt-get -y autoremove && apt-get -y install ufw fail2ban libwww-perl build-essential libtool automake autotools-dev autoconf pkg-config libssl-dev libgmp3-dev libevent-dev bsdmainutils libdb++-dev libminiupnpc-dev libboost-all-dev libqrencode-dev unzip && fallocate -l 4G /swapfile && chmod 600 /swapfile && mkswap /swapfile && swapon /swapfile && echo "vm.swappiness=10" >> /etc/sysctl.conf && echo "/swapfile none swap sw 0 0" >> /etc/fstab
-ufw allow 22/tcp && ufw allow $MNPORT/tcp && yes | ufw enable
 mkdir -p ~/coins && cd ~/coins && git clone https://github.com/legends420/GanjaCoin.git && cd ~/coins/GanjaCoin/src && make -f makefile.unix
 ~/coins/GanjaCoin/src/ganjacoind
 sudo cp ~/coins/GanjaCoin/MNSampleGanjaProject.conf ~/.Ganjaproject2/Ganjaproject.conf
@@ -49,7 +48,10 @@ addnode=91.121.95.32
 addnode=45.76.2.44
 EOF
 echo "starting node"
+ufw allow 22/tcp && ufw allow 12419/tcp && yes | ufw enable
 ~/coins/GanjaCoin/src/ganjacoind
+sleep 3
+echo
 echo "Generated Wallet masternode.conf:"
 echo
 echo $MNNAME $IPADDR:$RPCPORT $GENKEY $TXID $TXINDEX
