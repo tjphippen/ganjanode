6a42df3897a45bd5892b93892eb5b012043ae2f9
TJ Phippen <tj@tjphippen.com>
2018-06-29 23:21:46 -0700

    add genkey & connect/sync detection

--- ganjainstall.sh
+++ ganjainstall.sh
@@ -45,17 +45,17 @@ IPADDR=$(ip route get 8.8.8.8 | awk 'NR==1 {print $NF}')
 read -e -p "VPS IP Address: " -i "$IPADDR" IPADDR
 read -e -p "Choose Port: " -i "12419" MNPORT
 read -e -p "Choose RPC Port: " -i "12420" RPCPORT
+read -e -p "Choose RPC User: " RPCUSER
+read -e -p "Choose RPC Password: " RPCPASS
+read -e -p "Transaction ID: " TXID
+read -e -p "TXINDEX(0 or 1): " TXINDEX
+read -e -p "Wallet Masternode Name: " MNNAME
 read -rsp $'This will take a while so smoke a bowl & chill out, press any key to continue...\n' -n1 key
 apt-get update && apt-get -y upgrade && apt-get -y dist-upgrade && apt-get -y autoremove && apt-get -y install ufw fail2ban libwww-perl build-essential libtool automake autotools-dev autoconf pkg-config libssl-dev libgmp3-dev libevent-dev bsdmainutils libdb++-dev libminiupnpc-dev libboost-all-dev libqrencode-dev unzip && fallocate -l 4G /swapfile && chmod 600 /swapfile && mkswap /swapfile && swapon /swapfile && echo "vm.swappiness=10" >> /etc/sysctl.conf && echo "/swapfile none swap sw 0 0" >> /etc/fstab
 mkdir -p ~/coins && cd ~/coins && git clone https://github.com/legends420/GanjaCoin.git && cd ~/coins/GanjaCoin/src && make -f makefile.unix
 ~/coins/GanjaCoin/src/ganjacoind
+ufw allow 22/tcp && ufw allow $MNPORT/tcp && yes | ufw enable
 sudo cp ~/coins/GanjaCoin/MNSampleGanjaProject.conf ~/.Ganjaproject2/Ganjaproject.conf
-read -e -p "Choose RPC User: " RPCUSER
-read -e -p "Choose RPC Password: " RPCPASS
-read -e -p "Key(genkey): " GENKEY
-read -e -p "Transaction ID: " TXID
-read -e -p "TXINDEX(0 or 1): " TXINDEX
-read -e -p "Wallet Masternode Name: " MNNAME
 cat > ~/.Ganjaproject2/Ganjaproject.conf <<EOF
 rpcuser=$RPCUSER
 rpcpassword=$RPCPASS
@@ -72,27 +72,45 @@ maxconnections=500
 mnconflock=1
 masternode=1
 masternodeaddr=$IPADDR:$MNPORT
-masternodeprivkey=$GENKEY
+masternodeprivkey=MYGENKEY
+masternodeminprotocol=60029
 stake=0
 staking=0
 seednode=138.197.44.71
-addnode=192.99.55.111
-addnode=45.63.4.123
-addnode=81.152.218.253
-addnode=80.208.228.52
-addnode=104.131.15.51
-addnode=98.172.83.59
-addnode=216.173.152.247
-addnode=91.121.95.32
-addnode=45.76.2.44
+addnode=138.197.44.71
+addnode=45.32.212.138
+addnode=218.101.109.199
+addnode=73.196.26.253
+addnode=104.236.217.59
+addnode=216.172.33.74
+addnode=173.249.26.81
 EOF
-echo "starting node"
-ufw allow 22/tcp && ufw allow 12419/tcp && yes | ufw enable
+echo -n "Connecting to network"
+~/coins/GanjaCoin/src/ganjacoind
+sleep 5
+while [ "$(~/coins/GanjaCoin/src/ganjacoind getconnectioncount)" == "0" ]; do
+echo -n ".";
+sleep 5;
+done
+echo
+echo -n "Syncing chain"
+while [ "$(curl -s 'http://explorer.ganjacoinpro.com/api/getblockcount')" != "$(~/coins/GanjaCoin/src/ganjacoind getblockcount)" ]; do
+echo -n ".";
+sleep 5;
+done
+echo
+echo "Connected!"
+echo "Generating Masternode key.."
+GENKEY=$(~/coins/GanjaCoin/src/ganjacoind masternode genkey)
+sed -i '16s/.*/masternodeprivkey=$GENKEY/' ~/.Ganjaproject2/Ganjaproject.conf
+~/coins/GanjaCoin/src/ganjacoind stop
+sleep 3
 ~/coins/GanjaCoin/src/ganjacoind
 sleep 3
 echo
-echo "Generated Wallet masternode.conf:"
+echo "Copy the Generated Wallet line to you local/cold wallet masternode.conf file:"
 echo
 echo $MNNAME $IPADDR:$MNPORT $GENKEY $TXID $TXINDEX
 echo
+echo "Then open My Masternodes in your local/cold wallet & click start!"
 echo "Enjoy GanjaProject"
